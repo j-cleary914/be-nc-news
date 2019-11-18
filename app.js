7a8961ae31b2089173cae78094e39cbe29eaf593
j-cleary914 <j.cleary914@gmail.com>
2019-11-18 11:20:21 +0000

    installed cors

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const apiRouter = require("./Routes/apiRouter");
 const { handleCustomErrors, handlePSQLErrors } = require("./errors/index");
+const cors = require("cors");
+
+app.use(cors());
 
 app.use(express.json());
 
